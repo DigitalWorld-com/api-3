d92972a812c96baf653c20708941487b9bce928c
AxelVillegasLuna <villegas_axel@hotmail.com>
2023-06-04 13:44:48 -0300

    Saludo desde grupo 3

---
+++ api/src/main/java/com/grupo3/api/controllers/GreetingController.java
@@ -0,0 +1,16 @@
+package com.grupo3.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Estoy saludando desde el grupo 3!";
+    }
+
+}
