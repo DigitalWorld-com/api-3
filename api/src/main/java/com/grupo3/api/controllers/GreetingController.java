89340e1f51ac8ebf49c249cc9657f60763f61e44
AxelVillegasLuna <villegas_axel@hotmail.com>
2023-06-14 15:01:15 -0300

    Configuracion de puerto y cambio de archivo cors

--- api/src/main/java/com/grupo3/api/controllers/GreetingController.java
+++ api/src/main/java/com/grupo3/api/controllers/GreetingController.java
@@ -3,14 +3,14 @@ package com.grupo3.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/api-3")
+@RequestMapping("/api3")
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting() {
         return "Estoy saludando desde el grupo 3!";
     }
-
 }
